a8cc7fcefc7e509f5c1132f4a4e7307032cec7d7
AnnaMmcc <urosevicanja913@gmail.com>
2025-02-11 10:30:55 +0100

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::view("/welcomee", "welcomee");
+
+Route::view("/about", "about");
+
+Route::view("/shop", "shop");
+
+Route::view("/contact", "contact");
+
